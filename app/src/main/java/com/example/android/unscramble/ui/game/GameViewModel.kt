8b6a7179b132b5f41d6f45ac95cd4c2f2a88f422
Alex Lee <alexlee78980@gmail.com>
2022-06-27 10:04:40 -0700

    added scramble word function

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,6 +3,7 @@ package com.example.android.unscramble.ui.game
 import android.icu.lang.UCharacter.GraphemeClusterBreak.V
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import java.net.HttpURLConnection
 
 class GameViewModel: ViewModel() {
     private var wordsList: MutableList<String> = mutableListOf()
@@ -24,7 +25,19 @@ class GameViewModel: ViewModel() {
     }
     private fun getNextWord(){
         currentWord = allWordsList.random()
-        val tempWord = currentWord.toCharArray()
+        Can't finish GitHub sharing process
+        Successfully created project 'unscramble' on GitHub, but initial push failed:
+        Invocation failed Unexpected end of file from server
+                java.lang.RuntimeException: Invocation failed Unexpected end of file from server
+        at git4idea.GitAppUtil.sendXmlRequest(GitAppUtil.java:30)
+        at git4idea.http.GitAskPassApp.main(GitAskPassApp.java:58)
+        Caused by: java.net.SocketException: Unexpected end of file from server
+        at java.base/sun.net.www.http.HttpClient.parseHTTPHeader(HttpClient.java:866)
+        at java.base/sun.net.www.http.HttpClient.parseHTTP(HttpClient.java:689)
+        at java.base/sun.net.www.http.HttpClient.parseHTTPHeader(HttpClient.java:863)
+        at java.base/sun.net.www.http.HttpClient.parseHTTP(HttpClient.java:689)
+        at java.base/sun.net.www.protocol.http.HttpURLConnection.getInputStream0(HttpURLConnection.java:1615)
+        at java.base/sun.net.www.protocol.http.HttpURLConnection.getInputStream(HttpURLConnection.java:1.    val tempWord = currentWord.toCharArray()
         while (String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
